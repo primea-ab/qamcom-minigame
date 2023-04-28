d1f46d5453010f68513d402aab3cc35847501ec5
sirmackan <marcus@meeloo.se>
2023-04-28 09:55:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -109,137 +109,156 @@ const collides = (obj1, obj2) => {
     obj1.y + obj1.height > obj2.y;
 }
 
+let previousTimeStamp;
+
 // game loop
-const loop = () => {
-  requestAnimationFrame(loop);
-  context.clearRect(0,0,canvas.width,canvas.height);
-
-  if (leftPaddle.y > ball.y) {
-    leftPaddle.dy = -paddleSpeed;
-  } else {
-    leftPaddle.dy = paddleSpeed;
+const loop = (timestamp) => {
+  if (previousTimeStamp === undefined) {
+    previousTimeStamp = timestamp;
   }
 
-  // move paddles by their velocity
-  leftPaddle.y += leftPaddle.dy;
-  rightPaddle.y += rightPaddle.dy;
+  const elapsed = timestamp - previousTimeStamp;
 
-  // prevent paddles from going through walls
-  if (leftPaddle.y < grid) {
-    leftPaddle.y = grid;
-  }
-  else if (leftPaddle.y > maxPaddleY) {
-    leftPaddle.y = maxPaddleY;
-  }
+  if (elapsed !== 0) {
+    context.clearRect(0,0,canvas.width,canvas.height);
 
-  if (rightPaddle.y > wantedPosition) {
-    rightPaddle.dy = -paddleSpeed;
-  } else if (rightPaddle.y < wantedPosition) {
-    rightPaddle.dy = paddleSpeed;
-  }
+    if (leftPaddle.y > ball.y) {
+      leftPaddle.dy = -paddleSpeed;
+    } else {
+      leftPaddle.dy = paddleSpeed;
+    }
 
-  if (rightPaddle.y < grid) {
-    rightPaddle.y = grid;
-  }
-  else if (rightPaddle.y > maxPaddleY) {
-    rightPaddle.y = maxPaddleY;
-  }
+    // move paddles by their velocity
+    leftPaddle.y += leftPaddle.dy * elapsed;
 
-  // draw paddles
-  context.fillStyle = 'white';
-  context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
-  context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+    if (rightPaddle.y <= wantedPosition && rightPaddle.y + rightPaddle.dy * elapsed >= wantedPosition) {
+      rightPaddle.y = wantedPosition;
+    } else if (rightPaddle.y >= wantedPosition && rightPaddle.y + rightPaddle.dy * elapsed <= wantedPosition) {
+      rightPaddle.y = wantedPosition;
+    } else {
+      rightPaddle.y += rightPaddle.dy * elapsed;
+    }
 
-  // move ball by its velocity
-  if (gameStarted) {
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-  }
+    // prevent paddles from going through walls
+    if (leftPaddle.y < grid) {
+      leftPaddle.y = grid;
+    }
+    else if (leftPaddle.y > maxPaddleY) {
+      leftPaddle.y = maxPaddleY;
+    }
 
-  // prevent ball from going through walls by changing its velocity
-  if (ball.y < grid) {
-    ball.y = grid;
-    ball.dy *= -1;
-  }
-  else if (ball.y + grid > canvas.height - grid) {
-    ball.y = canvas.height - grid * 2;
-    ball.dy *= -1;
-  }
+    if (rightPaddle.y > wantedPosition) {
+      rightPaddle.dy = -paddleSpeed;
+    } else if (rightPaddle.y < wantedPosition) {
+      rightPaddle.dy = paddleSpeed;
+    }
 
-  // reset ball if it goes past paddle (but only if we haven't already done so)
-  if ( (ball.x < 0 || ball.x > canvas.width) && !ball.resetting) {
-    ball.resetting = true;
+    if (rightPaddle.y < grid) {
+      rightPaddle.y = grid;
+    }
+    else if (rightPaddle.y > maxPaddleY) {
+      rightPaddle.y = maxPaddleY;
+    }
 
-    // give some time for the player to recover before launching the ball again
-    setTimeout(() => {
-      //counter.score = 0;
-      //counterRef.innerHTML = 0;
-      health -= 1;
-      if (health === 1) {
-        document.getElementById('health-2').style = HIDE;
-      } else if (health === 0) {
-        fetch('https://pong.fly.dev/scoreboard').then(async (res) => {
-          const data = await res.json();
-          if (data.length === 0) {
-            let elem = document.createElement('p');
-            elem.textContent = 'No one on the scoreboard yet. Be the first to join!';
-            elem.classList.add('no-scoreboard');
-            document.getElementById('scoreboard-list').appendChild(elem);
-            document.getElementById('scoreboard-list').classList.remove('two-column-list');
-          } else {
-            data.forEach(user => {
-              let elem = document.createElement('li');
-              elem.textContent = `${user.name} - ${user.score} points`;
+    // draw paddles
+    context.fillStyle = 'white';
+    context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+    context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+
+    // move ball by its velocity
+    if (gameStarted) {
+      ball.x += ball.dx * elapsed;
+      ball.y += ball.dy * elapsed;
+    }
+
+    // prevent ball from going through walls by changing its velocity
+    if (ball.y < grid) {
+      ball.y = grid;
+      ball.dy *= -1;
+    }
+    else if (ball.y + grid > canvas.height - grid) {
+      ball.y = canvas.height - grid * 2;
+      ball.dy *= -1;
+    }
+
+    // reset ball if it goes past paddle (but only if we haven't already done so)
+    if ( (ball.x < 0 || ball.x > canvas.width) && !ball.resetting) {
+      ball.resetting = true;
+
+      // give some time for the player to recover before launching the ball again
+      setTimeout(() => {
+        //counter.score = 0;
+        //counterRef.innerHTML = 0;
+        health -= 1;
+        if (health === 1) {
+          document.getElementById('health-2').style = HIDE;
+        } else if (health === 0) {
+          fetch('https://pong.fly.dev/scoreboard').then(async (res) => {
+            const data = await res.json();
+            if (data.length === 0) {
+              let elem = document.createElement('p');
+              elem.textContent = 'No one on the scoreboard yet. Be the first to join!';
+              elem.classList.add('no-scoreboard');
               document.getElementById('scoreboard-list').appendChild(elem);
-            })
-          }
-          document.getElementById('start-game').style = HIDE;
-          document.getElementById('logo').style = HIDE;
-          document.getElementById('scoreboard').style = SHOW;
-        })
-        cancelAnimationFrame(canvasId);
-        return;
-      }
+              document.getElementById('scoreboard-list').classList.remove('two-column-list');
+            } else {
+              data.forEach(user => {
+                let elem = document.createElement('li');
+                elem.textContent = `${user.name} - ${user.score} points`;
+                document.getElementById('scoreboard-list').appendChild(elem);
+              })
+            }
+            document.getElementById('start-game').style = HIDE;
+            document.getElementById('logo').style = HIDE;
+            document.getElementById('scoreboard').style = SHOW;
+          })
+          cancelAnimationFrame(canvasId);
+          return;
+        }
 
-      ball.resetting = false;
-      ball.x = canvas.width / 2;
-      ball.y = canvas.height / 2;
-      //send the ball away from the player when restarting
-      ball.dx = -ball.dx;
-    }, 400);
+        ball.resetting = false;
+        ball.x = canvas.width / 2;
+        ball.y = canvas.height / 2;
+        //send the ball away from the player when restarting
+        ball.dx = -ball.dx;
+      }, 400);
+    }
+
+    // check to see if ball collides with paddle. if they do change x velocity
+    if (collides(ball, leftPaddle)) {
+      ball.dx *= -1;
+
+      // move ball next to the paddle otherwise the collision will happen again
+      // in the next frame
+      ball.x = leftPaddle.x + leftPaddle.width;
+    }
+    else if (collides(ball, rightPaddle)) {
+      counter.score += 1;
+      counterRef.innerHTML = counter.score;
+      ball.dx *= -1;
+
+      // move ball next to the paddle otherwise the collision will happen again
+      // in the next frame
+      ball.x = rightPaddle.x - ball.width;
+    }
+
+    // draw ball
+    context.fillStyle = 'white';
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    // draw walls
+    context.fillStyle = 'lightgrey';
+    context.fillRect(0, 0, canvas.width, grid);
+    context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
+
+    // draw dotted line down the middle
+    for (let i = grid; i < canvas.height - grid; i += grid * 2) {
+      context.fillRect(canvas.width / 2 - grid / 2, i, grid/4, grid);
+    }
   }
-
-  // check to see if ball collides with paddle. if they do change x velocity
-  if (collides(ball, leftPaddle)) {
-    ball.dx *= -1;
-
-    // move ball next to the paddle otherwise the collision will happen again
-    // in the next frame
-    ball.x = leftPaddle.x + leftPaddle.width;
-  }
-  else if (collides(ball, rightPaddle)) {
-    counter.score += 1;
-    counterRef.innerHTML = counter.score;
-    ball.dx *= -1;
-
-    // move ball next to the paddle otherwise the collision will happen again
-    // in the next frame
-    ball.x = rightPaddle.x - ball.width;
-  }
-
-  // draw ball
-  context.fillStyle = 'white';
-  context.fillRect(ball.x, ball.y, ball.width, ball.height);
-
-  // draw walls
-  context.fillStyle = 'lightgrey';
-  context.fillRect(0, 0, canvas.width, grid);
-  context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
-
-  // draw dotted line down the middle
-  for (let i = grid; i < canvas.height - grid; i += grid * 2) {
-    context.fillRect(canvas.width / 2 - grid / 2, i, grid/4, grid);
-  }
+  
+  previousTimeStamp = timestamp;
+  requestAnimationFrame(loop);
 }
 
 addEventListener("touchmove", (event) => {
